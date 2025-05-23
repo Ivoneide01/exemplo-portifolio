41a951aafb0e362ee5eb1b9670da0f0725fa857c
Nid silva <80358224+Ivoneide01@users.noreply.github.com>
2025-05-23 03:39:41 -0300

    teste

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// Importar as funções necessárias do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js";
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// Configuração do Firebase
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBmqnjiAHb-pjqsspZ6kcBjcK9Lks0iHqU",
   authDomain: "portifolionid.firebaseapp.com",
@@ -15,7 +14,7 @@ const firebaseConfig = {
   appId: "1:438566113511:web:efaf522682c2d6d8051242"
 };
 
-// Inicializar o aplicativo Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
